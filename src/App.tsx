6f892888c94bf9d1cb0a8c6ad9635b8a2aa3640e
김광회 <postelian@gmail.com>
2025-12-21 16:37:18 +0900

    로그인 마무리

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   useEffect(() => {
     if (headerRef.current) setHeaderHeight(headerRef.current.offsetHeight);
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    !getAuthToken() ? navigate('/') : navigate('/posts');
+    if (!getAuthToken()) navigate('/');
   }, [navigate]);
 
   const logout = () => {
@@ -32,28 +32,30 @@ function App() {
     <GlobalLayer>
       <div className="w-screen h-screen dark:text-gray-100 bg-gray-100 dark:bg-gray-800">
         <div ref={headerRef} className="navbar p-2 bg-base-300 shadow-md">
-          <div>
+          <div className="min-w-48">
             <Link to="/" className="btn btn-ghost text-xl">
               FE Hiring Project
             </Link>
           </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link to="/board" className="btn btn-ghost">
-                  게시판
-                </Link>
-              </li>
-              <li>
-                <Link to="/charts" className="btn btn-ghost">
-                  차트
-                </Link>
-              </li>
-            </ul>
+          <div className="w-full flex justify-between items-center">
+            <div style={{ display: getAuthToken() ? 'flex' : 'none' }} className="w-1/2 flex-none">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <Link to="/posts" className="btn btn-ghost">
+                    게시판
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/charts" className="btn btn-ghost">
+                    차트
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <button style={{ display: getAuthToken() ? 'flex' : 'none' }} className="btn btn-ghost" onClick={logout}>
+              로그아웃
+            </button>
           </div>
-          <button style={{ display: getAuthToken() ? 'flex' : 'none' }} className="btn btn-ghost" onClick={logout}>
-            로그아웃
-          </button>
         </div>
 
         <main className="p-4" style={{ height: winHeight - headerHeight }}>
