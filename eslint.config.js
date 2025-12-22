06f9cf776daf7e45435d87d869529af5c2ea3c77
김광회 <postelian@gmail.com>
2025-12-22 12:50:12 +0900

    lint 설정 보완

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,15 +16,44 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      import: importPlugin, // 'import' 플러그인 등록
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
-      semi: ['error', 'always'],
-      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-use-before-define': 'off',
+      camelcase: 'warn',
+      'max-nested-callbacks': 'off',
+      'class-methods-used': 'off',
+      'import/prefer-default-export': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          ts: 'never',
+          jsx: 'never',
+          tsx: 'never',
+        },
+      ],
+      'no-underscore-dangle': 0,
       'no-unused-vars': 'off', // 기본 규칙 비활성화
+      // react
+      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'warn',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rule-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      // typescript
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-use-before-define': 2,
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
